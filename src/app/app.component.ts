04869c7153ebaa7e84ad8b95995dd70a4d7eccae
Alaa <mir.alaa96@gmail.com>
2019-05-30 13:33:18 -0400

    seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@
    Copyright (c) 2019 MDBootstrap.com
 */ 
 import { Component } from '@angular/core';
+import { SEOService } from './services/SEO/seo.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-title='FIT IT SOlutions'
+constructor(private seo:SEOService){}
+
 }
