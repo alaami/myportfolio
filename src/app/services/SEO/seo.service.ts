87cf9a1b8d78891a6f9416a137c4173260a4622b
Alaa <mir.alaa96@gmail.com>
2019-05-30 16:36:33 -0400

    Add icon + edit Seo service

--- src/app/services/SEO/seo.service.ts
+++ src/app/services/SEO/seo.service.ts
@@ -8,13 +8,11 @@ export class SEOService {
 
   constructor(private meta: Meta) {
     meta.addTags([
-       {name: 'description', content: 'Fit IT Solutions is recognized by creativity, innovation and quality. We offer exceptional services in Web development, Web application integration, managed service and consulting. Our expertise will establish a proposal of the best options available on the market to help you achieve your specific goals We ensure the provision of adequate solutions that meet your needs and respect your budgets.'},   
-       {name: 'viewport', content: 'width=device-width, initial-scale=1'},   
+       {name: 'description', content: 'Fit IT Solutions is recognized by creativity, innovation and quality. We offer exceptional services in Web development, Web application integration, managed service and consulting. Our expertise will establish a proposal of the best options available on the market to help you achieve your specific goals We ensure the provision of adequate solutions that meet your needs and respect your budgets.'},
        {name: 'robots', content: 'INDEX, FOLLOW'},
        {name: 'author', content: 'Fit IT Solutions'},
        {name: 'keywords', content: 'TypeScript, Angular, PHP, Web development, Web companies, Web agency, Web agencies, IT Solutions, Information Technology, Technical Support, it Services, it Consultant, Managed Service Provider, it Companies, it Solutions, it Security, it Manager, Managed it Services,Technology Consulting, Cloud Architecture,Tech Solutions, it Service Management, Cloud Infrastructurs'},
-       {httpEquiv: 'Content-Type', content: 'text/html'},
-       {charset: 'UTF-8'}
+       {httpEquiv: 'Content-Type', content: 'text/html'}
     ]);     
 }
 }
