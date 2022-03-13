595a47bf93a5e05594ef6197a9a3f13eb2e0392d
alaamir <mir.alaa96@gmail.com>
2022-03-13 16:17:55 -0400

    Correct meta descriptiom

--- src/app/services/SEO/seo.service.ts
+++ src/app/services/SEO/seo.service.ts
@@ -8,7 +8,7 @@ export class SEOService {
 
   constructor(private meta: Meta) {
     meta.addTags([
-       {name: 'description', content: 'Fit IT Solutions is recognized by creativity, innovation and quality. We offer exceptional services in Web development, Web application integration, managed service and consulting. Our expertise will establish a proposal of the best options available on the market to help you achieve your specific goals We ensure the provision of adequate solutions that meet your needs and respect your budgets.'},
+       {name: 'description', content: 'We offer services in Web, Mobile and Desktop App development. IT consulting. We offer best options on the market that meet your needs and respect your budgets.'},
        {name: 'robots', content: 'INDEX, FOLLOW'},
        {name: 'author', content: 'Fit IT Solutions'},
        {name: 'keywords', content: 'TypeScript, Angular, PHP, Web development, Web companies, Web agency, Web agencies, IT Solutions, Information Technology, Technical Support, it Services, it Consultant, Managed Service Provider, it Companies, it Solutions, it Security, it Manager, Managed it Services,Technology Consulting, Cloud Architecture,Tech Solutions, it Service Management, Cloud Infrastructurs'},
